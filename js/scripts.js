0e09c0be0b0d965f572d36e166e6f89e8bd381d9
renan <renanalves23@outlook.com>
2019-11-23 19:54:40 -0300

    Commit Inicial

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+
+// Menu Mobile
+
+
+
+
+// About us Tab
+
+var aboutUs = {
+  "Missão": "Fazer com que cada cliente seja reconhecido como autoridade em seu segmento de atuação. Agregar valor ao negócio, potencializar o crescimento das operações e promover e estreitar o relacionamento do cliente com o seu público alvo, por meio da geração de conteúdo de relevância.",
+  "Visão": "Ser reconhecida pelos clientes e pelo mercado como uma empresa parceira, inovadora e criativa, que oferece sempre os melhores produtos e soluções em Comunicação Empresarial Integrada.",
+  "Valores": "<ul><li>Comprometimento</li><li>Inovação</li><li>Ética profissional</li><li>Superação dos resultados</li><li>Melhoria contínua</li></ul>"
+};
+
+var unseletected_color = "#646872";
+var seletected_color = "#2A2D34";
+
+
+
+// Slider de serviços
+
+var our_services = [
+  {
+    'title': 'Webdesign',
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus tincidunt sem non sodales. Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi.'
+  },
+
+  {
+    'title': 'Branding',
+    'text': 'Praesent finibus tincidunt sem non sodales. Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi.'
+  },
+
+  {
+    'title': 'Marketing Digital',
+    'text': 'Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus.'
+  }
+  
+];
+
+
+// Data Footer
+
+
+
+  
+   
+
+
+   
